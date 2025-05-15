e2b45b21ffb458268425206eb22cef8311ff779c
MostafaAbobaker <35193789+MostafaAbobaker@users.noreply.github.com>
2025-05-15 12:08:23 +0300

    add sub category

--- src/app/core/pages/add-sub-category/add-sub-category.component.ts
+++ src/app/core/pages/add-sub-category/add-sub-category.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
-import { MessageService } from 'primeng/api';
 import { GovernoratesService } from '../../services/governorates.service';
 import { IGovernorates } from '../../interface/igovernorates';
 import { ICategory } from '../../../features/interface/icategory';
@@ -10,10 +9,12 @@ import { CommonModule } from '@angular/common';
 import { MultiSelectModule } from 'primeng/multiselect';
 import { EditorModule } from 'primeng/editor';
 import { SubCategoryService } from '../../services/sub-category.service';
+import { MessageService } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-add-sub-category',
-  imports: [RouterModule, CommonModule, MultiSelectModule, EditorModule, ReactiveFormsModule],
+  imports: [RouterModule, CommonModule, MultiSelectModule, EditorModule, ReactiveFormsModule , ToastModule],
   templateUrl: './add-sub-category.component.html',
   styleUrl: './add-sub-category.component.scss',
   providers: [MessageService]
@@ -23,27 +24,39 @@ export class AddSubCategoryComponent {
   text: string | undefined;
   categories!: ICategory[]
   governorates!: IGovernorates[]
+
   formSubCategory!: FormGroup;
+
   imageP: File | undefined;
   images: any[] = [];  // Array to hold image files and their preview URLs
 
-  constructor(private _subCategoryService: SubCategoryService, private _mainServices: MainServicesService, private _governoratesService: GovernoratesService,
-    private fb: FormBuilder, private messageService: MessageService) {
+
+  constructor(
+    private _subCategoryService: SubCategoryService,
+    private messageService: MessageService,
+    private _mainServices: MainServicesService,
+    private _governoratesService: GovernoratesService,
+    private fb: FormBuilder,
+  ) {
     this.formSubCategory = this.fb.group({
+      nameAr: ['', [Validators.required, Validators.maxLength(100)]],
+      categoryId: ['', Validators.required],
+      LocationIds: [[]],
+      showHome: [false],
+      descAr: ['', [Validators.required, Validators.maxLength(500)]],
+      detailAr: ['', Validators.required]
 
     })
   }
 
   ngOnInit(): void {
-    this.showCategory();
-    this.showGovernorates();
+    this.loadCategories();
+    this.loadGovernorates();
   }
-  showCategory() {
+  loadCategories() {
     this._mainServices.getMainServices().subscribe({
       next: (response) => {
-        console.log(response);
         this.categories = response.data;
-        console.log(response.data);
       },
       error: (err) => {
         console.log(err);
@@ -51,10 +64,9 @@ export class AddSubCategoryComponent {
     });
   }
 
-  showGovernorates() {
+  loadGovernorates() {
     this._governoratesService.getAllGovernorates().subscribe({
       next: (response) => {
-        console.log(response);
         this.governorates = response.data;
       },
       error: (err) => {
@@ -64,17 +76,65 @@ export class AddSubCategoryComponent {
   }
 
   addSubCategory() {
+    const formData = new FormData();
 
+    formData.append('nameAr', this.formSubCategory.value.nameAr);
+    formData.append('descAr', this.formSubCategory.value.descAr);
+    formData.append('detailAr', this.formSubCategory.value.detailAr);
+    formData.append('categoryId', this.formSubCategory.value.categoryId);
+    formData.append('showHome', this.formSubCategory.value.showHome);
+    formData.append('videoUrl', this.formSubCategory.value.videoUrl);
+    formData.append('id', '0');
+    this.formSubCategory.value.LocationIds.forEach((id: string | Blob) => {
+      formData.append('LocationIds', id);
+    });
+    this.images.forEach(image => {
+      formData.append('files', image.file, image.file.name);  // append each image to FormData
+    });
+
+
+    this.formSubCategory.patchValue({ id: 0 });
+    console.log(formData);
+
+    /* this._subCategoryService.addSubCategory(formData.values).subscribe({
+      next: (response) => {
+        console.log(response);
+        this.messageService.add({ severity: 'success', summary: 'Success', detail: response.message });
+      },
+      error: (err) => {
+        console.log(err);
+        this.messageService.add({ severity: 'error', summary: 'Error', detail: err.message });
+
+      },
+    }); */
   }
 
 
 
 
+  removeImage(id:number) {
+   this.images = this.images.filter(img => img.id !== id);
 
+  }
 
 
   onFileImageSelected(event: any): void {
-    this.images = event.target.files[0];
+    // this.images = event.target.files[0];
+
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      Array.from(input.files).forEach(file => {
+        const reader = new FileReader();
+        reader.onload = (e: any) => {
+          this.images.push({
+            id: Math.random().toString(36).substring(2),
+            file,
+            previewUrl: e.target.result
+          });
+        };
+        reader.readAsDataURL(file);
+      });
+    }
   }
 
 

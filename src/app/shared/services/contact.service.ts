45be65fe4716289893f769c934ccef1dc71838c7
MostafaAbobaker <35193789+MostafaAbobaker@users.noreply.github.com>
2025-06-21 12:26:58 +0300

    edit service

--- src/app/shared/services/contact.service.ts
+++ src/app/shared/services/contact.service.ts
@@ -1,19 +1,19 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Observable, of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
+  private readonly cacheContactInfo = 'ContactInfo';
+  private isBrowser: boolean;
 
-
-  constructor(private _http: HttpClient ){
-
+  constructor(private _http: HttpClient, @Inject(PLATFORM_ID) private platformId: Object) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
   }
-
-<<<<<<< HEAD
-  getContact(): Observable<any> {
+    getContact(): Observable<any> {
     return this._http.get('ContactInfo/GetContactInfo');
   }
 
@@ -24,13 +24,18 @@ export class ContactService {
         return of(JSON.parse(cachedData));
       }
     }
-=======
->>>>>>> EditHeader
-
-
-  getContact(): Observable<any> {
-  return this._http.get('ContactInfo/GetContactInfo')
+    return this._http.get('ContactInfo/GetContactInfo').pipe(
+      tap(data => {
+        if (this.isBrowser) {
+          sessionStorage.setItem(this.cacheContactInfo, JSON.stringify(data));
+        }
+      })
+    );
   }
 
-
+  clearContactInfoCache(): void {
+    if (this.isBrowser) {
+      sessionStorage.removeItem(this.cacheContactInfo);
+    }
+  }
 }

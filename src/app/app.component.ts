c7df92d44f63252257ff8fb25015a969a8697c4c
MostafaAbobaker <35193789+MostafaAbobaker@users.noreply.github.com>
2025-06-23 10:27:24 +0300

    edit header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ScrollTopServiceService } from './shared/services/scroll-top-service.service';
+import { InfoService } from './info.service';
+import { IContact } from './shared/interface/icontact';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +12,22 @@ import { ScrollTopServiceService } from './shared/services/scroll-top-service.se
 })
 export class AppComponent {
   title = 'friyoon';
+  info:IContact = {} as IContact;
+  constructor(private _infoService:InfoService) {}
 
+  ngOnInit() {
+    this.getInfo();
+  }
+  getInfo(){
+    this._infoService.getInfo().subscribe({
+      next:(res)=>{
+        console.log('Info fetched successfully:', res.data);
+        this.info = res.data;
+      },
 
+      error:(err)=>{
+        console.log('Error fetching info:', err);
+      }
+    })
+  }
 }
